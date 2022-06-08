d6847468c30cc1041181faff72ff6c013b125302
zzfyx <1491944538@qq.com>
2022-06-08 22:17:04 +0800

    ，，，，，，，，，，，，，动画

--- app/src/main/java/com/hcmes/viewdemo/l2/Lin2Activity.kt
+++ app/src/main/java/com/hcmes/viewdemo/l2/Lin2Activity.kt
@@ -2,6 +2,8 @@ package com.hcmes.viewdemo.l2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.animation.*
 import com.hcmes.viewdemo.R
@@ -42,16 +44,48 @@ class Lin2Activity : AppCompatActivity() {
         rotate.repeatMode=Animation.REVERSE
 
         var set=AnimationSet(true)
+        set.interpolator=CycleInterpolator(1f)
         set.addAnimation(scaleAnimation)
         set.addAnimation(rotate)
 
-        var tran=TranslateAnimation(Animation.ABSOLUTE,-80f,Animation.ABSOLUTE,80f,Animation.ABSOLUTE,-80f,Animation.ABSOLUTE,80f)
+        var tran=TranslateAnimation(Animation.RELATIVE_TO_SELF,0f,Animation.RELATIVE_TO_SELF,0f,Animation.ABSOLUTE,80f,Animation.ABSOLUTE,480f)
         tran.fillAfter=true
-        tran.duration=1000
-        tran.repeatCount=Animation.INFINITE
-        tran.repeatMode=Animation.REVERSE
+        tran.duration=5000
+        tran.repeatCount=0
+        tran.fillAfter=true
+        tran.interpolator=BounceInterpolator()
         set.addAnimation(tran)
         binding.imageView.startAnimation(set)
+       Handler(Looper.myLooper()!!).postDelayed({
+                              set.cancel()
+            //如果设置的是多次循环的，则停止不了
+            rotate.cancel()
+        },2000)
+        var scale=ScaleAnimation(1f,1.2f,1f,1.2f,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f)
+        scale.fillAfter=true
+        scale.interpolator=BounceInterpolator()
+        scale.duration=6000
+        binding.imageView2.startAnimation(scale)
 
+        var loading=RotateAnimation(0f,360f,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f)
+        loading.interpolator=LinearInterpolator()
+        loading.repeatCount=Animation.INFINITE
+        loading.repeatMode=Animation.RESTART
+        loading.duration=2000
+        binding.imageViewLoading.startAnimation(loading)
+
+
+
+        var scan1= AnimationUtils.loadAnimation(this,R.anim.scale_alpha_anim)
+        var scan2= AnimationUtils.loadAnimation(this,R.anim.scale_alpha_anim)
+        var scan3= AnimationUtils.loadAnimation(this,R.anim.scale_alpha_anim)
+        var scan4= AnimationUtils.loadAnimation(this,R.anim.scale_alpha_anim)
+        binding.circlel.startAnimation(scan1)
+        scan2.startOffset=600
+        binding.circlel2.startAnimation(scan2)
+        scan3.startOffset=1200
+        binding.circlel3.startAnimation(scan3)
+        scan4.startOffset=1800
+        binding.circlel4.startAnimation(scan4)
     }
 }
