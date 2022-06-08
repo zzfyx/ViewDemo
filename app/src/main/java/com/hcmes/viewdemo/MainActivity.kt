1d16b29f2b14365c17fb26c410a5e8ac5e1f8d75
1491944538@qq.com <1491944538@qq.com>
2022-06-08 14:16:02 +0800

    第一次提交...1111

--- app/src/main/java/com/hcmes/viewdemo/MainActivity.kt
+++ app/src/main/java/com/hcmes/viewdemo/MainActivity.kt
@@ -9,6 +9,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
         var intent=Intent(baseContext,Lin1Activity::class.java)
         startActivity(intent)
     }
