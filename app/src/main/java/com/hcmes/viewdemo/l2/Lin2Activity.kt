dcde6546d5cf5d19046c4f23f9a30bda73a7c5b2
1491944538@qq.com <1491944538@qq.com>
2022-06-08 18:36:33 +0800

    第4次提交

--- app/src/main/java/com/hcmes/viewdemo/l2/Lin2Activity.kt
+++ app/src/main/java/com/hcmes/viewdemo/l2/Lin2Activity.kt
@@ -3,9 +3,7 @@ package com.hcmes.viewdemo.l2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
-import android.view.animation.ScaleAnimation
+import android.view.animation.*
 import com.hcmes.viewdemo.R
 import com.hcmes.viewdemo.databinding.ActivityL2Binding
 
@@ -15,6 +13,7 @@ class Lin2Activity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityL2Binding.inflate(LayoutInflater.from(this))
         setContentView(binding.root)
+        //xml文件用法
         var animation= AnimationUtils.loadAnimation(this,R.anim.scale)
         binding.imageView.startAnimation(animation)
 
@@ -30,7 +29,29 @@ class Lin2Activity : AppCompatActivity() {
         android:fillAfter="true"
         android:interpolator="@android:anim/accelerate_decelerate_interpolator"*/
         var scaleAnimation=ScaleAnimation(0.0f,1.4f,0f,1.4f,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f)
-        binding.imageView.startAnimation(scaleAnimation)
+        scaleAnimation.fillAfter=true
+        scaleAnimation.duration=1000
+        scaleAnimation.repeatCount=Animation.INFINITE
+        scaleAnimation.repeatMode=Animation.REVERSE
+
+        var rotate=RotateAnimation(0f,360f,Animation.RELATIVE_TO_SELF,0.5f,Animation.RELATIVE_TO_SELF,0.5f)
+
+        rotate.fillAfter=true
+        rotate.duration=1000
+        rotate.repeatCount=Animation.INFINITE
+        rotate.repeatMode=Animation.REVERSE
+
+        var set=AnimationSet(true)
+        set.addAnimation(scaleAnimation)
+        set.addAnimation(rotate)
+
+        var tran=TranslateAnimation(Animation.ABSOLUTE,-80f,Animation.ABSOLUTE,80f,Animation.ABSOLUTE,-80f,Animation.ABSOLUTE,80f)
+        tran.fillAfter=true
+        tran.duration=1000
+        tran.repeatCount=Animation.INFINITE
+        tran.repeatMode=Animation.REVERSE
+        set.addAnimation(tran)
+        binding.imageView.startAnimation(set)
 
     }
 }
